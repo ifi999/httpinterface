bdd5c2e2362885d6e3d5565e809d69d5436a5253
ifi999 <kkwkjm4231@gmail.com>
2023-04-22 19:13:19 +0900

    feat: getOembedData method 추가

--- src/main/java/com/example/httpinterface/presentation/OEmbedController.java
+++ src/main/java/com/example/httpinterface/presentation/OEmbedController.java
@@ -1,9 +1,14 @@
 package com.example.httpinterface.presentation;
 
 import com.example.httpinterface.application.OEmbedService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class OEmbedController {
 
     private final OEmbedService oEmbedService;
@@ -11,4 +16,10 @@ public class OEmbedController {
     public OEmbedController(OEmbedService oEmbedService) {
         this.oEmbedService = oEmbedService;
     }
+
+    @GetMapping("/oembed")
+    public ResponseEntity<?> getOembedData(@RequestParam @NonNull String requestUrl) {
+        return ResponseEntity.ok(oEmbedService.getOEmbedData(requestUrl));
+    }
+
 }
